f879e392eba71dab9bb967b1490f85f68e7eecd6
Melissa Miller <melissa.miller@pantheon.io>
2026-02-04 15:50:45 -0800

    Add GlobalWrapper

--- projects/test-project/page.tsx
+++ projects/test-project/page.tsx
@@ -1,13 +1,19 @@
+'use client';
+
+import { GlobalWrapper } from '@pantheon-systems/pds-toolkit-react';
+
 import styles from './page.module.css';
 
 export default function TestProject() {
 	return (
-		<div className={styles.container}>
-			<h2>Test Project</h2>
-			<p>
-				This is a test project to verify the dynamic routing works correctly.
-			</p>
-			<p>You can now start building your prototype here!</p>
-		</div>
+		<GlobalWrapper>
+			<div className={styles.container}>
+				<h2>Test Project</h2>
+				<p>
+					This is a test project to verify the dynamic routing works correctly.
+				</p>
+				<p>You can now start building your prototype here!</p>
+			</div>
+		</GlobalWrapper>
 	);
 }
